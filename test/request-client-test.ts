88519b70ddd1db95acd946299ef7842cb52af1a1
Spencer Tuft <spencer.tuft@gmail.com>
2019-10-01 11:22:54 -0600

    Completed coverage in test suite

--- test/request-client-test.ts
+++ test/request-client-test.ts
@@ -1,18 +1,21 @@
 import {assert} from 'chai';
 import {SinonStub, stub} from 'sinon';
+import * as wso2 from '@byu-oit/wso2-request'
 import ClaimsRequest from '../lib'
 
 describe('Assertions WSO2 Request Client', () => {
     let stubRequest: SinonStub;
-    let client: ClaimsRequest;
+    let client1: ClaimsRequest;
+    let client2: ClaimsRequest;
 
     beforeEach(() => {
-        stubRequest = stub(ClaimsRequest, 'makeRequest');
-        client = new ClaimsRequest('fakeUrl', {
+        stubRequest = stub(wso2, 'request');
+        client1 = new ClaimsRequest('fakeUrl', {
             clientKey: 'fakeKey',
             clientSecret: 'fakeSecret',
             wellKnownUrl: 'fakeWellKnown'
         });
+        client2 = new ClaimsRequest('fakeUrl');
     });
 
     afterEach(() => {
@@ -20,12 +23,12 @@ describe('Assertions WSO2 Request Client', () => {
     });
 
     it('will make a request to the getConcepts endpoint of the specified url', () => {
-        let c = client.info();
+        let c = client1.info({}, 'fakeJWT');
         assert.equal(stubRequest.callCount, 1);
     });
 
     it('will make a request to the verify endpoint of the specified url', async () => {
-        let c = client
+        let c = client2
             .assert('1')
             .subject('John')
             .mode('all')
